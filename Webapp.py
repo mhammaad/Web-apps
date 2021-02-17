721875f6dca52ffaccab48f0b758f1f05c64197f
m-hammaad <49973583+m-hammaad@users.noreply.github.com>
2021-02-17 22:00:22 +0100

    Update Webapp.py

--- Webapp.py
+++ Webapp.py
@@ -29,8 +29,8 @@ import locale
 import matplotlib.ticker as mtick
 
 #locale.setlocale(locale.LC_ALL, 'de_DE')
-locale.setlocale(locale.LC_NUMERIC, "german")
-#locale.setlocale(locale.LC_ALL, 'de_DE.utf8')
+#locale.setlocale(locale.LC_NUMERIC, "german")
+locale.setlocale(locale.LC_ALL, 'de_DE.utf8')
 font = {'family': 'calibri',
         'size' : 16}
 mpl.rc('font', **font)
